330473ea5dd2d69fba4dc1c186e41f1ec1d9d5c7
silvanoguel <silvanoguel@msn.com>
2023-04-11 15:14:26 +0200

    add: generate email function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
 const { createApp } = Vue;
 
 createApp({
   data() {
     return {
-     
+      emails: [],
     };
   },
-
   methods: {
-
+    generateEmail() {
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then((resp) => {
+            this.emails.push(resp.data.response);
+          });
+      }
+    },
   },
-}).mount("#app");
+  mounted() {
+    this.generateEmail();
+  },
+}).mount('#app');
